f128a056f575968764f3247e43fba7e143502953
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2026-03-03 13:09:36 -0500

    commented out example project

--- website-hosting-iac.go
+++ website-hosting-iac.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
-	"github.com/aws/constructs-go/constructs/v10"
+	// "github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
 
@@ -11,33 +11,33 @@ type WebsiteHostingIacStackProps struct {
 	awscdk.StackProps
 }
 
-func NewWebsiteHostingIacStack(scope constructs.Construct, id string, props *WebsiteHostingIacStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
+// func NewWebsiteHostingIacStack(scope constructs.Construct, id string, props *WebsiteHostingIacStackProps) awscdk.Stack {
+// 	var sprops awscdk.StackProps
+// 	if props != nil {
+// 		sprops = props.StackProps
+// 	}
+// 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+// 	// The code that defines your stack goes here
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("WebsiteHostingIacQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+// 	// example resource
+// 	// queue := awssqs.NewQueue(stack, jsii.String("WebsiteHostingIacQueue"), &awssqs.QueueProps{
+// 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
+// 	// })
 
-	return stack
-}
+// 	return stack
+// }
 
 func main() {
 	defer jsii.Close()
 
 	app := awscdk.NewApp(nil)
 
-	NewWebsiteHostingIacStack(app, "WebsiteHostingIacStack", &WebsiteHostingIacStackProps{
-		awscdk.StackProps{
-			Env: env(),
-		},
-	})
+	// NewWebsiteHostingIacStack(app, "WebsiteHostingIacStack", &WebsiteHostingIacStackProps{
+	// 	awscdk.StackProps{
+	// 		Env: env(),
+	// 	},
+	// })
 
 	app.Synth(nil)
 }
